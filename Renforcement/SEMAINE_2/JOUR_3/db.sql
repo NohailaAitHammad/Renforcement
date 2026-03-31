5cb54f39f3e0442d7eccf1124ad08d60fda5c6c2
n1o2h <aythmadnhylt@gmail.com>
2026-03-31 21:55:00 +0200

    challenge 6 et 7 du our jeudi semaine 2

---
+++ Renforcement/SEMAINE_2/JOUR_3/db.sql
@@ -0,0 +1,61 @@
+CREATE TABLE utilisateurs (
+    id INT PRIMARY KEY  AUTo_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM("Apprenant", "Professeur")
+)ENGINE=INNODB;
+
+CREATE TABLE apprenants (
+    id INT PRIMARY KEY  AUTO_INCREMENT,
+    CNE VARCHAR(50),
+    filliere VARCHAR(50)
+)ENGINE=INNODB;
+
+CREATE TABLE professeurs (
+    id INT PRIMARY KEY  AUTO_INCREMENT,
+    matricule VARCHAR(50)
+)ENGINE=INNODB;
+
+CREATE TABLE cours (
+    id INT PRIMARY KEY  AUTO_INCREMENT,
+    nom VARCHAR(50) UNIQUE,
+    prix FLOAT CHECK ( prix>0 )
+    professeur_id INT,
+    FOREIGN KEY (professeur_id) REFERENCES  professeurs (id) ON DELETE  CASCADE
+)engine=INNODB;
+
+CREATE TABLE inscription (
+    id INT PRIMARY KEY  AUTO_INCREMENT,
+    apprenant_id INT,
+    cour_id INT,
+    date_inscription TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (apprenant_id) REFERENCES  apprenants(id) ON DELETE  CASCADE ,
+    FOREIGN KEY (cour_id) REFERENCES  cours(id) ON DELETE  CASCADE ,
+)ENGINE=INNODB;
+
+CREATE TABLE modules (
+    id INT PRIMARY KEY  AUTO_INCREMENT,
+    nom VARCHAR(50),
+    cour_id INT,
+    FOREIGN KEY (cour_id) REFERENCES  cours(id) ON DELETE  CASCADE
+)ENGINE=INNODB;
+
+CREATE TABLE lessons (
+    id INT PRIMARY KEY  AUTO_INCREMENT,
+    titre VARCHAR(50),
+    module_id INT,
+    FOREIGN KEY (module_id) REFERENCES  modules(id) ON DELETE  CASCADE
+)ENGINE=INNODB;
+
+
+CREATE TABLE categories (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR5(50)
+)ENGINE=INNODB;
+
+CREATE TABLE avis (
+    id INT PRIMARY KEY  AUTO_INCREMENT,
+    comment VARCHAR(50),
+    note INT CHECK ( note >= 1 AND note <= 5 ),
+)ENGINE=INNODB;
--- Renforcement/SEMAINE_2/JOUR_4/db.sql
+++ Renforcement/SEMAINE_2/JOUR_4/db.sql
@@ -24,7 +24,7 @@ CREATE TABLE ventes(
     livre_id INT,
     client VARCHAR(50),
     quantite INT DEFAULT 0,
-    date_vente TIMETAMP DEFAULT  CURRENT_TIMESTAMP,
+    date_vente TIMESTAMP DEFAULT  CURRENT_TIMESTAMP,
     FOREIGN KEY (livre_id) REFERENCES livres(id) ON DELETE  CASCADE
 )ENGINE=INNODB;
 
@@ -41,7 +41,7 @@ ORDER BY nbrLivres DESC;
 
 -- 3
 SELECT g.nom , SUM(l.prix * v.quantite) AS "CHIFFRE D'AFFAIRE" FROM genres g
-JOIN livres l ON g.id = l.id
+JOIN livres l ON g.id = l.genre_id
 JOIN ventes v on l.id = v.livre_id
 GROUP BY g.id;
 
@@ -54,12 +54,89 @@ WHERE v.id IS NULL;
 
 -- 5
 
-SELECT l.id, l.nom,g.nom , COUNT(v.livre_id) AS "NOMBRE DE LIVRES VENDU" FROM genres g
-JOIN livres l ON g.nom = l.id
+SELECT l.id, l.nom,g.nom , SUM(v.quantite) AS total FROM genres g
+JOIN livres l ON g.id = l.genre_id
 JOIN ventes v on l.id = v.livre_id
-GROUP BY l.id;
+GROUP BY l.id
+HAVING total = (
+    SELECT MAX(total_ventes)
+    FROM (
+             SELECT SUM(v2.quantite) AS total_ventes
+             FROM livres l2
+                      JOIN ventes v2 ON l2.id = v2.livre_id
+             WHERE l2.genre_id = l.genre_id
+             GROUP BY l2.id
+         ) AS t
+);
 
 -- 6
+SELECT v.client FROM ventes v
+JOIN livres l ON v.livre_id = l.id
+GROUP BY v.client
+HAVING COUNT(DISTINCT  l.genre_id) > 3
+
+-- 7
+    SELECT MONTH(v.date_vente) AS mois, SUM(l.prix * v.quantite) AS CA
+    FROM ventes v
+    JOIN livres l ON v.livre_id = l.id
+    GROUP BY MONTH(v.date_vente);
+
+-- 8
+
+SELECT l.* FROM livres l
+WHERE l.annee_publication > 2020
+AND l.prix > (
+    SELECT AVG(prix) FROM livres WHERE genre_id = l.genre_id
+    );
+
+
+
+-- R-Challenge 7 — L'hôpital
+
+-- 1
+
+SELECT c.*, m.nom AS medecin, p.nom AS patient FROM medecins m
+JOIN consultations c ON m.id = c.medecin_id
+JOIN patients p ON c.patient_id = p.id;
+
+-- 2
+SELECT m.nom, COUNT(c.id) FROM medecins m
+JOIN consultations c ON c.medecin_id = m.id
+WHERE MONTH(c.date_consultation) = Month(CURRENT_DATE)
+GROUP BY m.id;
+
+-- 3
+SELECT p.nom, SUM(c.cout) AS total FROM patients p
+JOIN consultations c ON p.id = c.patient_id
+GROUP BY p.id;
+
+-- 4
+
+SELECT p.* FROM patients p
+LEFT JOIN consultations c ON c.patient_id = p.id
+WHERE c.id IS NULL;
+
+-- 5
+
+SELECT m.nom, COUNT(DISTINCT c.patient_id) AS nb_patients FROM medecins m
+JOIN consultations c ON c.medecin_id = m.id
+GROUP BY m.id
+ORDER BY nb_patients DESC
+LIMIT 1;
+
+-- 6
+SELECT pre.* , p.nom as patient, m.nom as medecin FROM patients p
+JOIN consultations c ON c.patient_id = p.id
+JOIN prescriptions pre ON pre.consultation_id = c.id
+JOIN medecins m ON c.medecin_id = m.id
+WHERE p.nom = "preee";
+
+-- 7
+SELECT m.specialite, COUNT(c.id) AS nb_const FROM medecins m
+JOIN consultations c ON m.id = c.medecin_id
+GROUP BY m.specialite
+ORDER BY nb_const DESC
+LIMIT 1;
 
 
 
